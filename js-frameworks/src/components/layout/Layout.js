9f7f9e3509c97959c1a26cfd9edb62e6e11aae52
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-10-12 18:18:40 +0200

    routes created

--- js-frameworks/src/components/layout/Layout.js
+++ js-frameworks/src/components/layout/Layout.js
@@ -1,19 +1,51 @@
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Home from '../home/Home';
+import About from '../about/About';
+import Contact from '../contact/Contact';
+import Login from '../login/Login';
 
 function Layout() {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Navbar.Brand href="/">React App</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/about/">About</Nav.Link>
-          <Nav.Link href="/contact/">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <Router>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <NavLink to="/" exact>
+          <Navbar.Brand>React App</Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <NavLink to="/" className="nav-link">
+              Home
+            </NavLink>
+            <NavLink to="/about" className="nav-link">
+              About
+            </NavLink>
+            <NavLink to="/contact" className="nav-link">
+              Contact
+            </NavLink>
+            <NavLink to="/login" className="nav-link">
+              Login
+            </NavLink>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Container>
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 
---
+++ js-frameworks/src/components/login/Login.js
@@ -0,0 +1,10 @@
+import Heading from '../layout/Heading';
+
+export default function Login() {
+  return (
+    <>
+      <Heading title="Login" />
+      <p>This is the Login Page</p>
+    </>
+  );
+}
