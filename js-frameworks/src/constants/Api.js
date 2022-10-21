39f070be9fdd9d00943a4f1495df15e382c3cd36
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-10-21 17:06:12 +0200

    httacess

--- js-frameworks/src/constants/Api.js
+++ js-frameworks/src/constants/Api.js
@@ -4,3 +4,5 @@ export const API_URL = "https://api.punkapi.com/v2/beers/";
 //export const API_URL = "https://noroff.herokuapp.com/v1/books/"
 // (test URL)
 
+//export const WP_URL = "http://javascript-frameworks.local/wp-json/"; 
+
